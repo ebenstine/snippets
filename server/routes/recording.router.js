3ddcb171da0a097ff9577606f00cd2d8f2a76616
Eben Stine <ebenstine@gmail.com>
2021-09-07 14:31:38 -0500

    recording router setup

---
+++ server/routes/recording.router.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const {rejectUnauthenticated } = require('../modules/authentication-middleware');
+router.get('/:id', rejectUnauthenticated, (req, res) => {
+    const id = req.params.id;
+  
+    const queryText = `
+                      SELECT * FROM recordings
+                      WHERE song_id = $1
+                      `
+  
+    pool.query(queryText, [id])
+    .then (result => {
+      console.log(result.rows);
+      res.send(result.rows)
+    })
+    .catch(err =>{ 
+      console.log('Error complete select song', err);
+    })
+  })
+  
+  
+  
+  router.post('/', rejectUnauthenticated, (req, res) => {
+    const recording = req.body;
+  
+    const queryText = `INSERT INTO "recordings" (
+                          song_id, src, description
+                       )
+                       VALUES ($1, $2, $3)`;
+    pool.query(queryText, [recording.song_id, recording.src, recording.description])
+  
+      .then(result => {
+        res.sendStatus(201);
+        console.log(result);
+  
+      }).catch(error => {
+        // catch for second query
+        console.log(error);
+        res.sendStatus(500)
+      })
+    })
+  
+  router.delete('/:id', rejectUnauthenticated, (req, res) => {
+    const id = req.params.id
+  
+    let sqlText = `DELETE FROM recordings 
+                   WHERE id = $1;`
+  
+    pool.query(sqlText, [id])
+        .then(result => {
+            console.log(result);
+            res.sendStatus(201);
+        }).catch(error => {
+            console.log('error in delete', error);
+            res.sendStatus(500);
+        });
+  });
+  
+  
+  module.exports = router;
