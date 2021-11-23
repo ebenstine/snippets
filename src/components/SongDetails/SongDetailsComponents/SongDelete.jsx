fb89e11a838c658c7667861468e47b7d80ddb699
Eben Stine <ebenstine@gmail.com>
2021-11-22 22:07:40 -0600

    styled songDelete dialog

--- src/components/SongDetails/SongDetailsComponents/SongDelete.jsx
+++ src/components/SongDetails/SongDetailsComponents/SongDelete.jsx
@@ -11,7 +11,7 @@ import Delete from '@material-ui/icons/Delete';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-    buttons: {
+    button: {
         marginLeft: '1em',
         marginBottom: '.3em',
         marginRight: '2em',
@@ -20,7 +20,21 @@ const useStyles = makeStyles({
         paddingLeft: '1em',
         paddingRight: '1em'
         
+    },
+    dialog: {
+        backgroundColor: '#efefef',
+        border: '1px solid #dd4e4e',
+        color: '#233d4d'
+    },
+
+    dialogButtons: {
+
+        color: '#233d4d',
+        border: '1px solid #c8ecf5',
     }
+    
+
+    
     
   })
   
@@ -28,7 +42,7 @@ const useStyles = makeStyles({
 
 function SongDelete({ song, dispatch }) {
   const [open, setOpen] = useState(false);
-  const {buttons} = useStyles();
+  const {button, dialog, dialogButtons} = useStyles();
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -51,20 +65,20 @@ function SongDelete({ song, dispatch }) {
 
   return (
     <>
-      <Button className={buttons} variant="contained" onClick={handleClickOpen}><Delete/></Button>
+      <Button className={button} variant="contained" onClick={handleClickOpen}><Delete/></Button>
       <Dialog open={open} onClose={handleClose} aria-labelledby="Rename song title input">
-        <DialogTitle id="dialogTitle">Delete This Song?</DialogTitle>
-        <DialogContent>
+        <DialogTitle className={dialog}>Delete This Song?</DialogTitle>
+        <DialogContent className={dialog} >
           <DialogContentText>
             Is the song saved somewhere permanent, or somewhere in the process of being published to a public platform?
             If not, maybe it's not really finished yet and should continue to live here.  What's the best move right now?
           </DialogContentText>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCancel} variant="outlined">
+        <DialogActions className={dialog}>
+          <Button className={dialogButtons} onClick={handleCancel} variant="contained">
             Cancel
           </Button>
-          <Button onClick={handleDelete} variant="outlined">
+          <Button className={dialogButtons} onClick={handleDelete} variant="contained">
             Delete
           </Button>
         </DialogActions>
