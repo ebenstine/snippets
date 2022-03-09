95cfa61c4b8fa580bd0334626f412fc671119676
Eben Stine <ebenstine@gmail.com>
2022-03-08 22:34:44 -0600

    finalized form updates and conditional dialog for user page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,6 +1,6 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSelector, useDispatch } from 'react-redux';
-import { Paper, Button, Typography, Card } from '@material-ui/core'
+import { Paper, Button, Typography, IconButton, Dialog} from '@material-ui/core'
 import useStyles from '../UserPage/UserPageStyles'
 import ExitToApp from '@material-ui/icons/ExitToApp';
 import AccountCircle from '@material-ui/icons/AccountCircle';
@@ -17,15 +17,15 @@ const UserPage = () => {
           songCount, 
           recordingCount, 
           button2, 
-          genrePrompt, 
           playIcon, 
           highlight,
-          infoCard
+          
         
         } = useStyles();
 
 
   const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
 
  
   useEffect(() => {
@@ -33,15 +33,32 @@ const UserPage = () => {
     dispatch({ type: 'FETCH_ALL_RECORDINGS '})
 }, [dispatch]);
 
+const handleClickOpen = () => {
+  setOpen(true);
+};
+
+const handleClose = () => {
+  setOpen(false)
+}
+
   return (
     <>
     
     <Paper className={paper} elevation={10}>
-      
-      <div className="container">
+    
+    
+    {songs.length ?  
+
+      <>
         <div style={{display:'flex', justifyContent:'center' }}>
+        
+        <IconButton
+          onClick={handleClickOpen}
+        >
         <AccountCircle style={{color: '#233d4d', width:30, height:30}}/>
+        </IconButton>
         </div>
+        
         <br></br>
 
         <Typography 
@@ -55,7 +72,17 @@ const UserPage = () => {
         </Typography>
 
         <br></br>
+        <Dialog
+        PaperProps={{
+          
+          style: { border: "1px solid #2a4f64" }
+
+        }}
+        open={open} 
+        onClose={handleClose}
+       
         
+        >
         <Typography 
         
           align="center" 
@@ -79,14 +106,36 @@ const UserPage = () => {
           &nbsp;You've uploaded <span className={highlight}>{allRecordings.length}</span> audio recordings.
       
         </Typography>
-      
-        <br></br>
-      
+        </Dialog>
+      </>
         
-        <br></br>
+
+      :
 
       
-      </div>
+      <>
+      <div style={{display:'flex', justifyContent:'center' }}>
+        <AccountCircle style={{color: '#233d4d', width:30, height:30}}/>
+        </div>
+
+        <Typography 
+          
+          align="center" 
+          variant = "h4" 
+          className = {welcome} 
+        >
+          
+          Hey, {user.username}!
+        </Typography>
+
+      </>
+      
+      }
+
+    
+
+      
+      
       <div style={{display:'flex', justifyContent:'center'}}>
       <AddAlbum/>
       </div>
@@ -102,8 +151,9 @@ const UserPage = () => {
           <ExitToApp/>
 
         </Button>
-
         </div>
+
+       
 
       </Paper>
     
